47eb0e2448c5a8b45ac7bd8079fab4f5cb92e0e8
Seba <sebastian1377@gmail.com>
2025-04-17 11:40:07 -0400

    no cache qfix01

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -17,7 +17,6 @@ type Status = {
 };
 
 export const fetchStatusDataByName = async (siteName: string) => {
-  export const dynamic = "force-dynamic";
   try {
     const site = await sql<Site[]>` 
       SELECT * FROM sites
